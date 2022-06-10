0ab3feabb00ffcb88afe89b28c66c507ea7a1047
sneha1413 <2003sneha2003@gmail.com>
2022-06-11 00:46:44 +0530

    Final Commit

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,24 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "APK stands for-",
+            "Under which of the following android is licensed?",
+            "Which of the following kernel is used in Android?",
+            "Does android support other languages than java?"
+    };
+    public static String choices[][]={
+            {"Android Phone Kit","Android Page Kit","Android Package Kit","None of the above"},
+            {"OSS","Sourceforge","Apache/MIT","None"},
+            {"MAC","Windows","Linux","Redhat"},
+            {"Yes","No","May be","Can't say"}
+
+    };
+    public static String correctAnswers[]={
+            "Android Package Kit",
+            "Apache/MIT",
+            "Linux",
+            "Yes"
+
+    };
+}
